681408200743327633e96a35ca717f69441a67b3
Kwanghyun Moon <mgh3326@naver.com>
2019-02-13 21:30:59 +0900

    Update gitignore

---
+++ hello-react/src/test.js
@@ -0,0 +1,7 @@
+var numbers = [1, 2, 3, 4, 5];
+// var processed = numbers.map(function (num) {
+//         return num * num;
+//     }
+// );
+var processed = numbers.map(num => num * num);
+console.log(processed);
